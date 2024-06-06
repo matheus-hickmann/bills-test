a506c7bc8505cdf11355a5c1ad9d49e0e7bb3540
Matheus Hickmann <hickmann.matheus@gmail.com>
2024-06-06 20:32:09 -0300

    Initial commit

---
+++ src/main/java/com/br/bills_test/BillsTestApplication.java
@@ -0,0 +1,13 @@
+package com.br.bills_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/bills_test/BillsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.bills_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
